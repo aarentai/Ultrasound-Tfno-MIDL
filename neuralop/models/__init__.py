9c2364d5192667e4857c867b6122551c85aca976
aarentai <haocheng.dai@utah.edu>
2023-03-25 10:54:27 -0600

    init

--- neuralop/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .tfno import TFNO3d, TFNO2d, TFNO1d, TFNO
-from .model_dispatcher import get_model
--- neuralop/training/__init__.py
+++
@@ -1,3 +0,0 @@
-from .losses import LpLoss, H1Loss
-from .trainer import Trainer
-from .torch_setup import setup
